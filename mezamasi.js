e26c8ad7ec6a47094427c4521d3bb5be392379fb
kasaimasato <2153064@stu.hus.ac.jp>
2015-09-14 16:40:07 +0900

    曲をinputからselectに変えて再生できた。

--- mezamasi.js
+++ mezamasi.js
@@ -1,13 +1,8 @@
 //背景関係
-bscd=new Array();
-bscd[0]="#ffa";
-bscd[1]="#fbf";
-bscd[2]="#fbb";
-bscd[3]="#afa";
-bscd[4]="#aff";
-bscd[5]="#bbf";
-bscd[6]="#ccc";
-ban_time=1; b=0; cnt=0;
+var bscd=["#ffa","#fbf","#fbb","#afa","#aff","#bbf","#ccc"];
+var ban_time=1;
+var b=0;
+var cnt=0;
 
 function banban_bgcolor()
         {
@@ -21,58 +16,58 @@ function banban_bgcolor()
 //アラーム関係
 var jsalarm={
 padfield:function(f){
-	return (f<10)? "0"+f : f
+	return (f<10)? "0"+f : f;
 },
 showcurrenttime:function(){
-	var dateobj=new Date()
-	var ct=this.padfield(dateobj.getHours())+":"+this.padfield(dateobj.getMinutes())+":"+this.padfield(dateobj.getSeconds())
-	this.ctref.innerHTML=ct
-	this.ctref.setAttribute("title", ct)
+	var dateobj=new Date();
+	var ct=this.padfield(dateobj.getHours())+":"+this.padfield(dateobj.getMinutes())+":"+this.padfield(dateobj.getSeconds());
+	this.ctref.innerHTML=ct;
+	this.ctref.setAttribute("title", ct);
 	if (typeof this.hourwake!="undefined"){ //if alarm is set
 		if (this.ctref.title==(this.hourwake+":"+this.minutewake+":"+this.secondwake)){
-			clearInterval(jsalarm.timer)
-			window.location=document.getElementById("musicloc").value
+			clearInterval(jsalarm.timer);
+			window.location=document.getElementById("audio_file").value;
 		}
 	}
 },
 init:function(){
-	var dateobj=new Date()
-	this.ctref=document.getElementById("jsalarm_ct")
-	this.submitref=document.getElementById("submitbutton")
+	var dateobj=new Date();
+	this.ctref=document.getElementById("jsalarm_ct");
+	this.submitref=document.getElementById("submitbutton");
 	this.submitref.onclick=function(){
-		jsalarm.setalarm()
-		this.value="アラームを設定"
-		this.disabled=true
-		return false
+		jsalarm.setalarm();
+		this.value="アラームを設定";
+		this.disabled=true;
+		return false;
 	}
-	this.resetref=document.getElementById("resetbutton")
+	this.resetref=document.getElementById("resetbutton");
 	this.resetref.onclick=function(){
-	jsalarm.submitref.disabled=false
-	jsalarm.hourwake=undefined
-	jsalarm.hourselect.disabled=false
-	jsalarm.minuteselect.disabled=false
-	jsalarm.secondselect.disabled=false
-	return false
+	jsalarm.submitref.disabled=false;
+	jsalarm.hourwake=undefined;
+	jsalarm.hourselect.disabled=false;
+	jsalarm.minuteselect.disabled=false;
+	jsalarm.secondselect.disabled=false;
+	return false;
 	}
-	var selections=document.getElementsByTagName("select")
-	this.hourselect=selections[0]
-	this.minuteselect=selections[1]
-	this.secondselect=selections[2]
+	var selections=document.getElementsByTagName("select");
+	this.hourselect=selections[0];
+	this.minuteselect=selections[1];
+	this.secondselect=selections[2];
 	for (var i=0; i<60; i++){
 		if (i<24) //If still within range of hours field: 0-23
-		this.hourselect[i]=new Option(this.padfield(i), this.padfield(i), false, dateobj.getHours()==i)
-		this.minuteselect[i]=new Option(this.padfield(i), this.padfield(i), false, dateobj.getMinutes()==i)
-		this.secondselect[i]=new Option(this.padfield(i), this.padfield(i), false, dateobj.getSeconds()==i)
+		this.hourselect[i]=new Option(this.padfield(i), this.padfield(i), false, dateobj.getHours()==i);
+		this.minuteselect[i]=new Option(this.padfield(i), this.padfield(i), false, dateobj.getMinutes()==i);
+		this.secondselect[i]=new Option(this.padfield(i), this.padfield(i), false, dateobj.getSeconds()==i);
 	}
-	jsalarm.showcurrenttime()
-	jsalarm.timer=setInterval(function(){jsalarm.showcurrenttime()}, 1000)
+	jsalarm.showcurrenttime();
+	jsalarm.timer=setInterval(function(){jsalarm.showcurrenttime()}, 1000);
 },
 setalarm:function(){
-	this.hourwake=this.hourselect.options[this.hourselect.selectedIndex].value
-	this.minutewake=this.minuteselect.options[this.minuteselect.selectedIndex].value
-	this.secondwake=this.secondselect.options[this.secondselect.selectedIndex].value
-	this.hourselect.disabled=true
-	this.minuteselect.disabled=true
-	this.secondselect.disabled=true
+	this.hourwake=this.hourselect.options[this.hourselect.selectedIndex].value;
+	this.minutewake=this.minuteselect.options[this.minuteselect.selectedIndex].value;
+	this.secondwake=this.secondselect.options[this.secondselect.selectedIndex].value;
+	this.hourselect.disabled=true;
+	this.minuteselect.disabled=true;
+	this.secondselect.disabled=true;
 }
-}
+};
